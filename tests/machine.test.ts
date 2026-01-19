b43624f159c76dd17be38eaa909f2a9e39a73936
Skye Elliot <actuallyori@gmail.com>
2026-01-19 13:37:04 +0000

    tests: Ensure setting room key backups downloaded flag works.

--- tests/machine.test.ts
+++ tests/machine.test.ts
@@ -1452,6 +1452,13 @@ describe(OlmMachine.name, () => {
             expect(progressListener).toHaveBeenCalledTimes(1);
             expect(progressListener).toHaveBeenCalledWith(0, 2, 1);
         });
+
+        test("can check and set if all room keys have been downloaded", async () => {
+            const m = await machine();
+            await expect(m.hasDownloadedAllRoomKeys(room)).resolves.toBe(false);
+            await m.setHasDownloadedAllRoomKeys(room);
+            await expect(m.hasDownloadedAllRoomKeys(room)).resolves.toBe(true);
+        });
     });
 
     describe("Request missing secrets", () => {
