524bfefa4d04e4aeb8b1118f7e7c284e4832378e
Damir Jelić <poljar@termina.org.uk>
2026-09-22 13:05:32 +0200

    Bump all of our dependencies

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
 
-use vergen_gitcl::{Emitter, GitclBuilder};
+use vergen_gitcl::{Emitter, Gitcl};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let gitcl = GitclBuilder::default().sha(true).describe(true, false, None).build()?;
+    let gitcl = Gitcl::builder().sha(true).describe(true, false, None).build();
     Emitter::default().add_instructions(&gitcl)?.emit()?;
 
     Ok(())
