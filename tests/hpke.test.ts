7c4ef4ed82d9a79d837c2655b034655f4771d226
Damir Jelić <poljar@termina.org.uk>
2026-09-25 10:38:22 +0200

    Don't require a string for the additional authenticated data in the HPKE module
    Instead, we accept a byte array which is the exact way vodozemac does it.

--- tests/hpke.test.ts
+++ tests/hpke.test.ts
@@ -13,21 +13,27 @@ describe("HPKE channel creation", () => {
         const alice = new HpkeSenderChannel();
         const bob = new HpkeRecipientChannel();
 
+        let aad = "AAD";
+        let aadBytes = new TextEncoder().encode(aad);
+
         const { message, channel: aliceUnidirectional } = alice.establishChannel(
             bob.publicKey,
             "It's a secret to everybody",
-            "AAD",
+            aadBytes,
         );
 
-        const { message: initialMessage, channel: bobUnidirectional } = bob.establishChannel(message, "AAD");
+        const { message: initialMessage, channel: bobUnidirectional } = bob.establishChannel(message, aadBytes);
         expect(initialMessage).toStrictEqual("It's a secret to everybody");
+
+        let aadTwo = "AAD2";
+        let aadTwoBytes = new TextEncoder().encode(aadTwo);
 
         const { initialResponse, channel: bobEstablished } = bobUnidirectional.establishBidirectionalChannel(
             "Initial response",
-            "AAD2",
+            aadTwoBytes,
         );
         const { initialResponse: secondPlaintext, channel: aliceEstablished } =
-            aliceUnidirectional.establishBidirectionalChannel(initialResponse, "AAD2");
+            aliceUnidirectional.establishBidirectionalChannel(initialResponse, aadTwoBytes);
 
         expect(secondPlaintext).toStrictEqual("Initial response");
 
@@ -37,8 +43,8 @@ describe("HPKE channel creation", () => {
         expect(aliceCheck.as_bytes()).toStrictEqual(bobCheck.as_bytes());
         expect(aliceCheck.to_digit()).toStrictEqual(bobCheck.to_digit());
 
-        const ciphertext = bobEstablished.seal("Other message", "");
-        const thirdPlaintext = aliceEstablished.open(ciphertext, "");
+        const ciphertext = bobEstablished.seal("Other message", []);
+        const thirdPlaintext = aliceEstablished.open(ciphertext, []);
 
         expect(thirdPlaintext).toStrictEqual("Other message");
     });
